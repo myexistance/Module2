67483b810bffa213e53920ca5166e18c36e98068
beulathamban <35990861+beulathamban@users.noreply.github.com>
2018-10-12 17:06:30 +1300

    Javascript introduction

---
+++ Javascript/js/script.js
@@ -0,0 +1,32 @@
+
+//output into the html for testing
+document.write("Hi, <h1>Welcome </h1> to learn Javascript. This is from the external file");
+
+//output as a pop up window/box
+alert("Hello everyone");
+
+//console.log for testing
+console.log("hello! I am console.log");
+
+var age1=35;
+var age2=40;
+var agePercy=20;
+alert(agePercy); //displays 20
+agePercy=40;
+//displays 40;
+//best common practice to display output
+document.getElementById("age").innerHTML=agePercy;//displays 40
+
+var myName="Beula";
+
+var word1="Hello"; //strings are a sequence of alphabets or special symbols
+var word2="Yoobee";
+var newWord=word1+word2;
+document.write(newWord); //string concatenation
+
+
+
+
+
+
+
